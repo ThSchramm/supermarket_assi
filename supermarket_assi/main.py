020aec74189876667a483644bb513d64bbe59c79
ThSchramm <thomas.schramm@rwth-aachen.de>
2021-01-17 17:18:42 +0100

    update table object funcs

--- supermarket_assi/main.py
+++ supermarket_assi/main.py
@@ -14,11 +14,23 @@ if __name__ == "__main__":
     products = table_object.TableObject(file_name='products')
     products_df = pandas.DataFrame()
 
-    products.read_resource()
+    groups = table_object.TableObject(file_name='groups')
+    groups_df = pandas.DataFrame()
 
-    products_df = products.get_data()
+    # Read all resources
+    if products.read_resource():
+        products_df = products.get_data()
+    else:
+        print('Fehler beim Lesen der Tabelle: ' + products.get_filename())
 
-    print( products_df )
+    if groups.read_resource():
+        groups_df = groups.get_data()
+    else:
+        print('Fehler beim Lesen der Tabelle: ' + groups.get_filename())
+
+    # Test
+    print(products_df)
+    print(groups_df)
 
     # Create the maingui, this is the first parent widget
     #
--- supermarket_assi/table_object.py
+++ supermarket_assi/table_object.py
@@ -1,5 +1,5 @@
-
 import pandas
+
 
 class TableObject:
     """
@@ -11,18 +11,31 @@ class TableObject:
     def __init__(self, file_name):
         self.file_name = file_name
         self.dataframe = pandas.DataFrame()
+        self.first_read = False
 
     def read_resource(self):
         """
-
-        :param xyz: xyz
+        Loads the source excel-table to the pandas dateaframe of the object
         """
         path = './data/' + self.file_name + '.xlsx'
-        self.dataframe = pandas.read_excel(path)
+        try:
+            if not self.first_read:
+                self.dataframe = pandas.read_excel(path)
+        except:
+            return False
+
+        return not self.dataframe.empty
 
     def get_data(self):
         """
-
+        Returns the dataframe of the table object.
         :return dataframe: Pandas dataframe of rode table
         """
         return self.dataframe
+
+    def get_filename(self):
+        """
+        Returns the filename of the source table.
+        :return file_name: Name of Table-Resource
+        """
+        return self.file_name
